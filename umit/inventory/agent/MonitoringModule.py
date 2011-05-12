c9df41f1dfef0eea2d78ef4f9ba2a40d483f05cd
Dragos Dena <dragos.dena@gmail.com>
2011-05-12 23:40:34 +0300

    Defined the Monitoring Module interface

---
+++ umit/inventory/agent/MonitoringModule.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2011 Adriano Monteiro Marques.
+#
+# Author: Dragos Dena <dragos.dena@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import threading
+from threading import Thread
+
+from umit.inventory.agent import Core
+
+class MonitoringModule(Thread):
+"""
+The interface which should be implemented by the monitoring modules.
+
+self.options: A dictionary with the options for this module which are
+read from the configuration file.
+"""
+
+    def __init__(self, configs, agent_main_loop):
+        Thread.__init__(self)
+
+        # Get the options from the configs and save them
+        self.options = dict()
+        module_config_options = configs.items(self.get_name())
+        for option in module_config_options:
+            self.options[option[0]] = self.options[option[1]]
+
+        # Save the agent Main Loop which will get the Module's messages
+        self.agent_main_loop = agent_main_loop
+
+
+    def get_name(self):
+        """Must be implemented by the Monitoring Module"""
+        pass
+
+
+    def send_message(self, message):
+        """
+        Used by the Monitoring Module which inherents this class
+        to send the message to the Notifications Server.
+        """
+        self.agent_main_loop.add_message(message)
+
+
+    def start(self):
+        """The Monitoring module main loop. Must be implemented."""
+        pass
+
+
